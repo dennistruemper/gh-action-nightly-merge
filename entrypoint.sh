72bd26b3e3b03877b079fa8d1fc6cd7c5385af46
dennistruemper <dennis.truemper@gmail.com>
2020-10-25 20:24:24 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,37 +6,11 @@ echo
 echo "  'Nightly Merge Action' is using the following input:"
 echo "    - stable_branch = '$INPUT_STABLE_BRANCH'"
 echo "    - development_branch = '$INPUT_DEVELOPMENT_BRANCH'"
-echo "    - allow_ff = $INPUT_ALLOW_FF"
-echo "    - allow_git_lfs = $INPUT_GIT_LFS"
-echo "    - ff_only = $INPUT_FF_ONLY"
-echo "    - allow_forks = $INPUT_ALLOW_FORKS"
 echo "    - user_name = $INPUT_USER_NAME"
 echo "    - user_email = $INPUT_USER_EMAIL"
-echo "    - push_token = $INPUT_PUSH_TOKEN = ${!INPUT_PUSH_TOKEN}"
 echo
 
-if [[ $INPUT_ALLOW_FORKS != "true" ]]; then
-  URI=https://api.github.com
-  API_HEADER="Accept: application/vnd.github.v3+json"
-  pr_resp=$(curl -X GET -s -H "${API_HEADER}" "${URI}/repos/$GITHUB_REPOSITORY")
-  if [[ "$(echo "$pr_resp" | jq -r .fork)" != "false" ]]; then
-    echo "Nightly merge action is disabled for forks (use the 'allow_forks' option to enable it)."
-    exit 0
-  fi
-fi
 
-if [[ -z "${!INPUT_PUSH_TOKEN}" ]]; then
-  echo "Set the ${INPUT_PUSH_TOKEN} env variable."
-  exit 1
-fi
-
-FF_MODE="--no-ff"
-if [[ "$INPUT_ALLOW_FF" == "true" ]]; then
-  FF_MODE="--ff"
-  if [[ "$INPUT_FF_ONLY" == "true" ]]; then
-    FF_MODE="--ff-only"
-  fi
-fi
 
 git remote set-url origin https://x-access-token:${!INPUT_PUSH_TOKEN}@github.com/$GITHUB_REPOSITORY.git
 git config --global user.name "$INPUT_USER_NAME"
@@ -50,10 +24,6 @@ git checkout $INPUT_STABLE_BRANCH
 git fetch origin $INPUT_DEVELOPMENT_BRANCH
 git checkout $INPUT_DEVELOPMENT_BRANCH
 
-if git merge-base --is-ancestor $INPUT_STABLE_BRANCH $INPUT_DEVELOPMENT_BRANCH; then
-  echo "No merge is necessary"
-  exit 0
-fi;
 
 set +o xtrace
 echo
@@ -63,12 +33,8 @@ echo
 set -o xtrace
 
 # Do the merge
-git merge $FF_MODE --no-edit $INPUT_STABLE_BRANCH
+git merge --ff-only --no-edit $INPUT_STABLE_BRANCH
 
-# Pull lfs if enabled
-if [[ $INPUT_GIT_LFS == "true" ]]; then
-  git lfs pull
-fi
 
 # Push the branch
 git push origin $INPUT_DEVELOPMENT_BRANCH
